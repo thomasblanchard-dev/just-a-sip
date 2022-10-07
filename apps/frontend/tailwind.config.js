f3eb5e22207e4579c82433e22b5965b140199265
Thomas B <thomas@krealab.fr>
2022-10-07 14:39:00 +0200

    feat(setup): setup project with next, lib, tailwind

---
+++ apps/frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+const { join } = require('path');
+
+// available since Nx v 12.5
+const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
+
+module.exports = {
+  presets: [require('../../tailwind-workspace-preset.js')],
+  content: [
+    join(
+      __dirname,
+      '{src,pages,components}/**/*!(*.stories|*.spec).{ts,tsx,html}',
+    ),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
