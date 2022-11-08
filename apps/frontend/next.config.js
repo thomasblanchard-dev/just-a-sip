1a9c52a3b5408acb8c9177ab1817fd83c5b52619
Thomas B <thomas@krealab.fr>
2022-11-08 14:32:12 +0100

    fix(nextjs): next 13 webpack issuer missing

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -7,6 +7,8 @@ const { withNx } = require('@nrwl/next/plugins/with-nx');
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   experimental: {
     appDir: true,
   },
