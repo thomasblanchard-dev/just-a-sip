91ad88a4a1553941dd9172d49b33fa7c48a9fd78
Thomas B <thomas@krealab.fr>
2023-02-01 11:32:42 +0100

    fix(storybook): relative path for files

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -22,4 +22,11 @@ module.exports = {
   features: {
     storyStoreV7: true,
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = './';
+    }
+
+    return config;
+  },
 };
