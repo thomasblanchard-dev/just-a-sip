588fa70184a7d440e9b746cf55003a5237395b97
Thomas B <thomas@krealab.fr>
2023-02-01 09:59:08 +0100

    chore(cleaning): remove unused config in vite

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,27 +1,7 @@
-const path = require('path');
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.tsx'),
-      name: 'Just a Sip Lib',
-      fileName: (format) => `index.${format}.js`,
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['react'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          react: 'React',
-        },
-      },
-    },
-  },
   plugins: [react()],
 });
