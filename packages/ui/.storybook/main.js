f9b307221026148f17cb89e3a8b6351e2e91b126
Thomas B <thomas@krealab.fr>
2023-02-01 11:40:57 +0100

    feat(storybook): add dark mode

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -6,6 +6,7 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    'storybook-dark-mode',
     {
       name: '@storybook/addon-postcss',
       options: {
